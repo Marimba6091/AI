b048ed0d578c7b315132be69f854fc75358be60b
Marimba6091 <artemkozlov20112826.2@gmail.com>
2025-11-27 20:00:29 +0700

    finish

--- number/Neuron_net.py
+++ number/Neuron_net.py
@@ -20,14 +20,14 @@ class NN:
         self.__b2 = np.zeros((16,))
         self.__b3 = np.zeros((10,))
 
-        self.__loss = None
+        self.__loss = 0
     
     @property
     def loss(self):
         return self.__loss
     
-    def set_the_best_data(self):
-        df = pandas.read_json("number/train_data.json")
+    def set_data_from(self, x):
+        df = pandas.read_json(x)
         self.__w1 = np.array(df["_NN__w1"][0])
         self.__w2 = np.array(df["_NN__w2"][0])
         self.__w3 = np.array(df["_NN__w3"][0])
@@ -36,11 +36,10 @@ class NN:
         self.__b3 = np.array(df["_NN__b3"][0])
     
     def write(self, loss):
-        if loss < pandas.read_json("number/train_data.json")["loss"][0]:
+        if loss < pandas.read_json("number/train_data.json")["_NN__loss"][0]:
             dataframe = {}
             for key, val in self.__dict__.items():
                 dataframe[key] = [val.tolist()]
-            dataframe["loss"] = [loss]
             df = pandas.DataFrame(dataframe)
             df.to_json("number/train_data.json")
 
@@ -52,7 +51,7 @@ class NN:
         return o
 
     def train(self, data):
-        epochs = 100000
+        epochs = 20000
         lmd = .01
         for epoch in range(epochs):
             for x, y_true in data:
@@ -81,25 +80,26 @@ class NN:
                 print(f"{self.__loss:.15f} - {epoch}")
                 self.write(self.__loss)
 
-nn = NN()
-is_train = bool(input("1>> "))
-is_the_best_data = not bool(input("2>> "))
-if is_the_best_data:
-    nn.set_the_best_data()
-if is_train:
-    file = open("number/data.txt")
-    file_data = []
-    for i in file:
-        f = i.split(" ")
-        try:
-            file_data.append((np.array(list(map(int, f[0].split(",")))), (np.array(list(map(int, f[1].split(",")))))))
-        except:
-            print(f[0], f[1])
-    nn.train(file_data)
-file = nrd.Img_to_matrix(Image.open("input.png")).get_data()
-test_input = np.array(file)
-pr = nn.predict(test_input)
-for x, i in enumerate(pr):
-    print(f"{x} - {i:.7f}")
-if bool(input("3>> ")):
-    nn.write(nn.loss)
+if __name__ == "__main__":
+    nn = NN()
+    is_train = bool(input("1>> "))
+    is_the_best_data = not bool(input("2>> "))
+    if is_the_best_data:
+        nn.set_data_from("number/train_data.json")
+    if is_train:
+        file = open("number/data.txt")
+        file_data = []
+        for i in file:
+            f = i.split(" ")
+            try:
+                file_data.append((np.array(list(map(int, f[0].split(",")))), (np.array(list(map(int, f[1].split(",")))))))
+            except:
+                print(f[0], f[1])
+        nn.train(file_data)
+    file = nrd.Img_to_matrix(Image.open("input.png")).get_data()
+    test_input = np.array(file)
+    pr = nn.predict(test_input)
+    for x, i in enumerate(pr):
+        print(f"{x} - {i:.7f}")
+    if bool(input("3>> ")):
+        nn.write(nn.loss)
--- number/pict.py
+++ number/pict.py
@@ -3,17 +3,17 @@ import normalize_data as nrd
 from PIL import Image
 
 file = "input.png"
-dct = {4:"0,0,0,0,1,0,0,0,0,0", 2:"0,0,1,0,0,0,0,0,0,0", 0:"1,0,0,0,0,0,0,0,0,0", 3:"0,0,0,1,0,0,0,0,0,0", 1:"0,1,0,0,0,0,0,0,0,0"}
-number = 1
+dct = {4:"0,0,0,0,1,0,0,0,0,0", 2:"0,0,1,0,0,0,0,0,0,0", 0:"1,0,0,0,0,0,0,0,0,0", 3:"0,0,0,1,0,0,0,0,0,0", 1:"0,1,0,0,0,0,0,0,0,0", 5:"0,0,0,0,0,1,0,0,0,0", 6:"0,0,0,0,0,0,1,0,0,0", 7:"0,0,0,0,0,0,0,1,0,0", 8:"0,0,0,0,0,0,0,0,1,0", 9:"0,0,0,0,0,0,0,0,0,1"}
+number = 4
 b = os.path.getsize(file)
 while True:
     if b != os.path.getsize(file):
         try:
             img = Image.open(file)
-            b = os.path.getsize(file)
             with open("number/data.txt", "a") as data:
                 data.write(f"\n{",".join(list(map(str, nrd.Img_to_matrix(img).get_data())))} {dct[number]}")
             img.close()
             print(number)
+            b = os.path.getsize(file)
         except:
             print("error")
